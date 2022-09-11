5e91d6258b42083ad1354c665f10be653b680aa4
Alex Tzinov <alex.tzinov@gmail.com>
2022-09-11 14:21:08 -0600

    include more scripts

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.png";
 import { Main, Title } from "./components";
 import { DownloadAsAppCTA } from "./DownloadAsAppCTA";
+import logo from "./logo.png";
 import { StatusLight } from "./StatusLight";
 import { UpdateAppButton } from "./UpdateAppButton";
 import _versions from "./versions.json";
@@ -16,14 +16,11 @@ function App() {
       <img src={logo} className="logo" alt="logo" />
       <Title>
         PWA seed project 🎉
-        <div style={{ maxHeight: '200px', height: '200px', overflowY: 'scroll' }}>
-
-          {versions.map(v => (
-
+        <div style={{ maxHeight: "200px", height: "200px", overflowY: "scroll" }}>
+          {versions.map((v) => (
             <section className="mostRecentVersion">
               <div>
                 {/* eslint-disable-next-line */}
-                <code>// most recent build</code>
                 <code>date: {v?.date}</code>
                 <code>tag: {v?.tag}</code>
                 <code>message: {v?.message}</code>
@@ -32,7 +29,6 @@ function App() {
             </section>
           ))}
         </div>
-
         <section className="mostRecentVersion">
           <div>
             {/* eslint-disable-next-line */}
---
+++ tagRelease.sh
@@ -0,0 +1,1 @@
+jq -n --arg greeting $(git rev-parse HEAD) --arg date "$(date)" --arg tag $(git describe --tags --abbrev=0) --arg message "$(git log -1 --pretty=%B)" '[{"hash":$greeting, "date":$date, "tag": $tag, "message":$message}]' > current_version.json && jq -s 'add' src/App/versions.json current_version.json > blah.json && cat blah.json > src/App/versions.json
