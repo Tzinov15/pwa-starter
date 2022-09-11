6e8d9ba2d714b3451efb198e84435e40e3c2a755
Alex Tzinov <alex.tzinov@gmail.com>
2022-09-11 14:38:39 -0600

    one more style change

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -14,6 +14,7 @@ function App() {
       <img src={logo} className="logo" alt="logo" />
       <Title>
         PWA seed project 🎉
+        <br />
         <span>the cleanest template the world has ever seen, damn right</span>
         <div style={{ maxHeight: "200px", height: "200px", overflowY: "scroll" }}>
           {versions.map((v) => (
